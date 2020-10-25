e41d899a64a884720aa65438265cf33b4d8ad9e6
imufiid <imufiid98@gmail.com>
2020-10-25 10:37:48 +0700

    update process text prepocessing

--- ProcessText.php
+++ ProcessText.php
@@ -4,8 +4,9 @@ class ProcessText
 {
     function index()
     {
-        require_once('lib/TextPrepocessing.php');
+        require_once('lib/core/TextPrepocessing.php');
         require_once('lib/Tfidf.php');
+        include 'koneksi.php';
 
         echo "<h2>Text Mining Metode TF-IDF</h2>";
         echo "<br><br>";
@@ -13,22 +14,75 @@ class ProcessText
         $tfidf = new Tfidf();
 
         // CASE FOLDING
-        if ($textPrepocessing->caseFolding() == 1) {
+        mysqli_query($conn, "TRUNCATE case_folding");
+        $queryCaseFolding = mysqli_query($conn, "SELECT * FROM documents") or die(mysqli_error($conn));
+
+        foreach ($queryCaseFolding as $key => $val) {
+            $id_dok         = $val['doc_id'];
+            $kalimat_asli   = $val['complaint'];
+            $kode_disposisi = $val['code_disposition'];
+
+            $caseFolding = $textPrepocessing->caseFolding($kalimat_asli);
+            $masukan1 = mysqli_query($conn, "INSERT INTO case_folding VALUES('','$caseFolding','$id_dok','$kode_disposisi')");
+        }
+
+        if ($masukan1 == 1) {
             echo "> Case Folding berhasil";
             echo "<br>";
+
             // TOKENISASI
-            if ($textPrepocessing->tokenitation() == 1) {
+            mysqli_query($conn, "TRUNCATE token"); //kosongkan isi field
+            $queryTokenization = mysqli_query($conn, "SELECT * FROM case_folding") or die(mysqli_error($conn));
+
+            while ($row = mysqli_fetch_array($queryTokenization)) {
+                $hasilToken = [];
+                $kalimat_asli   = $row['case_folding'];
+                $id_dok         = $row['doc_id'];
+                $kode_disposisi = $row['code_disposition'];
+
+                $tokenization = $textPrepocessing->tokenization($kalimat_asli);
+                $masukkan2 = mysqli_query($conn, "INSERT INTO token VALUES('','$tokenization','$id_dok', '$kode_disposisi')");
+                unset($hasilToken);
+            }
+
+            if ($masukkan2 == 1) {
                 echo "> Tokenitation berhasil";
                 echo "<br>";
+
                 // FILTERING
-                if ($textPrepocessing->filtering() == 1) {
+                mysqli_query($conn, "TRUNCATE filtering");
+                $query = mysqli_query($conn, "SELECT * FROM token") or die(mysqli_error($conn));
+                while ($row = mysqli_fetch_array($query)) {
+                    $hasilFilter = [];
+                    $doc_id = $row['doc_id'];
+                    $kode_disposisi = $row['code_disposition'];
+
+                    $filtering = $textPrepocessing->filtering($row['term']);
+                    $masukkan3 = mysqli_query($conn, "INSERT INTO filtering VALUES('', '$filtering','$doc_id', '$kode_disposisi')");
+                    unset($hasilFilter);
+                }
+                if ($masukkan3 == 1) {
                     echo "> Filtering berhasil";
                     echo "<br>";
+
                     // STEMMING
-                    $stemming = $textPrepocessing->stemming();
-                    if ($stemming == 1) {
+                    mysqli_query($conn, "TRUNCATE stemming");
+                    $query = mysqli_query($conn, "SELECT * FROM filtering") or die(mysqli_error($conn));
+                    while ($row = mysqli_fetch_array($query)) {
+                        $hasilStem = [];
+                        $hasil;
+                        $kata             = $row['term'];
+                        $id_dok           = $row['doc_id'];
+                        $kode_disposisi   = $row['code_disposition'];
+
+                        $stemming = $textPrepocessing->stemming($kata);
+                        $masukkan4 = mysqli_query($conn, "INSERT INTO stemming VALUES('','$stemming','$id_dok','$kode_disposisi')");
+                        unset($hasilStem);
+                    }
+                    if ($masukkan4 == 1) {
                         echo "> Stemming berhasil.";
                         echo "<br>";
+
                         // HITUNG TF
                         if ($tfidf->hitungTf() == 1) {
                             echo "> Hitung TF berhasil";
